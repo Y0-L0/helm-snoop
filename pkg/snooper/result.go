3c48b041ae8d24fe6290ba6ba96aa4b709223b76
Johannes Lohmer <lohmer@univention.de>
2025-12-20 23:05:13 +0100

    feat(path): Make Path, Paths and Result json serializable

--- pkg/snooper/result.go
+++ pkg/snooper/result.go
@@ -9,3 +9,19 @@ type Result struct {
 	DefinedNotUsed path.Paths
 	UsedNotDefined path.Paths
 }
+
+// ResultsJSON is a compact JSON representation of a Result, exposing only stable path encodings.
+type ResultsJSON struct {
+	Referenced     path.PathsJSON `json:"referenced"`
+	DefinedNotUsed path.PathsJSON `json:"definedNotUsed"`
+	UsedNotDefined path.PathsJSON `json:"usedNotDefined"`
+}
+
+// ToJSON converts a Result into its compact, deterministic JSON representation.
+func (r *Result) ToJSON() ResultsJSON {
+	return ResultsJSON{
+		Referenced:     r.Referenced.ToJSON(),
+		DefinedNotUsed: r.DefinedNotUsed.ToJSON(),
+		UsedNotDefined: r.UsedNotDefined.ToJSON(),
+	}
+}
