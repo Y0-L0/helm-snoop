0ac713df9476659aa395bbb577b36122a09d1c50
Johannes Lohmer <jojo.dev@lohmer.com>
2026-01-10 09:15:47 +0100

    feat: Switch from POC CLI to production CLI implementation

--- cmd/helm-snoop/main_test.go
+++ cmd/helm-snoop/main_test.go
@@ -7,40 +7,46 @@ import (
 )
 
 func (s *GoldenTest) TestCLI_Usage() {
-	restore, _, err, code := interceptMain([]string{"helm-snoop"})
+	restore, _, stderr, code := interceptMain([]string{"helm-snoop"})
 	defer restore()
+
 	main()
-	s.Require().Equal(2, *code)
-	s.EqualGoldenFile("usage.golden.txt", err.Bytes())
+
+	s.Require().Equal(1, *code)
+	s.EqualGoldenFile("usage.golden.txt", stderr.Bytes())
 }
 
 func (s *GoldenTest) TestCLI_Missing() {
-	restore, _, err, code := interceptMain([]string{"helm-snoop", "does-not-exist"})
+	restore, stdout, _, code := interceptMain([]string{"helm-snoop", "does-not-exist"})
 	defer restore()
+
 	main()
+
 	s.Require().Equal(1, *code)
-	s.EqualGoldenFile("missing.golden.txt", err.Bytes())
+	s.EqualGoldenFile("missing.golden.txt", stdout.Bytes())
 }
 
 func (s *GoldenTest) TestCLI_TestChart() {
 	chartPath := filepath.Join(s.chartsDir, "test-chart")
-	restore, stdout, stderr, code := interceptMain([]string{"helm-snoop", chartPath, "debug"})
+	restore, stdout, stderr, code := interceptMain([]string{"helm-snoop", chartPath})
 	defer restore()
+
 	main()
-	s.Require().Equal(0, *code, stderr.String())
+
+	s.Require().Equal(1, *code, stderr.String())
 	s.EqualGoldenFile("test-chart.golden.txt", stdout.Bytes())
 }
 
-func interceptMain(args []string) (restore func(), out *bytes.Buffer, errBuf *bytes.Buffer, code *int) {
+func interceptMain(args []string) (restore func(), outBuf *bytes.Buffer, errBuf *bytes.Buffer, code *int) {
 	oldExit, oldOut, oldErr := osExit, stdout, stderr
-	outBuf, errB := &bytes.Buffer{}, &bytes.Buffer{}
-	stdout, stderr = outBuf, errB
+	outBuf, errBuf = &bytes.Buffer{}, &bytes.Buffer{}
+	stdout, stderr = outBuf, errBuf
 
 	var exitCode int
 	osExit = func(c int) { exitCode = c }
 	os.Args = args
 
-	return func() { osExit, stdout, stderr = oldExit, oldOut, oldErr }, outBuf, errB, &exitCode
+	return func() { osExit, stdout, stderr = oldExit, oldOut, oldErr }, outBuf, errBuf, &exitCode
 }
 
 func (s *GoldenTest) EqualGoldenFile(golfenFileName string, actual []byte) {
