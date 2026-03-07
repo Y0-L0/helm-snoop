3f8e628cbdbcf7b36222680dca7466d73253ec9c
Johannes Lohmer <lohmer@univention.de>
2026-03-07 21:55:16 +0100

    fix(tplparser): Mark and/or/not/all/any/empty/len/list args as Checked
    Wire emitCheckedNoResultFn for truthiness checks, emptiness checks, len,
    and list. Non-leaf paths used in these contexts are no longer falsely
    reported as Undefined.

--- pkg/tplparser/eval_refkind_test.go
+++ pkg/tplparser/eval_refkind_test.go
@@ -29,3 +29,51 @@ func (s *Unittest) TestRefKind_ElseIfConditionMarkedChecked() {
 	s.Equal(".b", paths[1].ID())
 	s.Equal(vpath.Checked, paths[1].Usage)
 }
+
+func (s *Unittest) TestRefKind_AndArgsMarkedChecked() {
+	paths := s.parse(`{{ if and .Values.a .Values.a.name }}yes{{ end }}`)
+
+	s.Require().Len(paths, 2)
+	s.Equal(".a", paths[0].ID())
+	s.Equal(vpath.Checked, paths[0].Usage)
+	s.Equal(".a.name", paths[1].ID())
+	s.Equal(vpath.Checked, paths[1].Usage)
+}
+
+func (s *Unittest) TestRefKind_OrArgsMarkedChecked() {
+	paths := s.parse(`{{ if or .Values.a .Values.b }}yes{{ end }}`)
+
+	s.Require().Len(paths, 2)
+	s.Equal(".a", paths[0].ID())
+	s.Equal(vpath.Checked, paths[0].Usage)
+	s.Equal(".b", paths[1].ID())
+	s.Equal(vpath.Checked, paths[1].Usage)
+}
+
+func (s *Unittest) TestRefKind_LenMarkedChecked() {
+	paths := s.parse(`{{ len .Values.items }}`)
+
+	s.Require().Len(paths, 1)
+	s.Equal(".items", paths[0].ID())
+	s.Equal(vpath.Checked, paths[0].Usage)
+}
+
+func (s *Unittest) TestRefKind_ListMarkedChecked() {
+	paths := s.parse(`{{ list .Values.a }}`)
+
+	s.Require().Len(paths, 1)
+	s.Equal(".a", paths[0].ID())
+	s.Equal(vpath.Checked, paths[0].Usage)
+}
+
+func (s *Unittest) TestRefKind_AndBodyRemainsConsumed() {
+	paths := s.parse(`{{ if and .Values.a .Values.b }}{{ .Values.name }}{{ end }}`)
+
+	s.Require().Len(paths, 3)
+	s.Equal(".a", paths[0].ID())
+	s.Equal(vpath.Checked, paths[0].Usage)
+	s.Equal(".b", paths[1].ID())
+	s.Equal(vpath.Checked, paths[1].Usage)
+	s.Equal(".name", paths[2].ID())
+	s.Equal(vpath.Consumed, paths[2].Usage)
+}
